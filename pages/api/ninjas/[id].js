9ab3d72ed07d80ae18cb37bd787332e574991484
stephenlaichaowen <stephenlaitest@gmail.com>
2021-02-17 13:46:26 +0800

    commit

--- pages/api/ninjas/[id].js
+++ pages/api/ninjas/[id].js
@@ -8,8 +8,8 @@ export default async (req, res) => {
   switch (method) {
     case 'GET':
       try {
-        // const ninjas = JSON.parse(fs.readFileSync(path.resolve('./public/data.json')))        
-        const ninjas = JSON.parse(fs.readFileSync('data.json'))
+        const ninjas = JSON.parse(fs.readFileSync(path.resolve('./public/data.json')))        
+        // const ninjas = JSON.parse(fs.readFileSync('data.json'))
         const ninja = ninjas.find(ninja => ninja.id === req.query.id)
         // console.log(ninja)
         res.json(ninja)
@@ -26,7 +26,8 @@ export default async (req, res) => {
       console.log(req.query.id)
 
       try {
-        let ninjas = JSON.parse(fs.readFileSync('data.json'))
+        let ninjas = JSON.parse(fs.readFileSync(path.resolve('./public/data.json')))        
+        // let ninjas = JSON.parse(fs.readFileSync('data.json'))
         
         const foundIndex = ninjas.findIndex(ninja => ninja.id === id);
         ninjas[foundIndex] = { id, name, email, website, city }
@@ -46,13 +47,15 @@ export default async (req, res) => {
       break
     case 'DELETE':
       try {
-        fs.readFile('data.json', (err, data) => {
+        // fs.readFile('data.json', (err, data) => {
+        fs.readFile(fs.readFileSync(path.resolve('./public/data.json')), (err, data) => {
           if (err) throw err
 
           let ninjas = JSON.parse(data)
           ninjas = ninjas.filter(ninja => ninja.id !== req.query.id)
 
-          fs.writeFile('data.json', JSON.stringify(ninjas, null, 2), (err) => {
+          // fs.writeFile('data.json', JSON.stringify(ninjas, null, 2), (err) => {
+          fs.writeFile(fs.readFileSync(path.resolve('./public/data.json')), JSON.stringify(ninjas, null, 2), (err) => {
             if (err) throw err;
             console.log('Data written to file');
           });
